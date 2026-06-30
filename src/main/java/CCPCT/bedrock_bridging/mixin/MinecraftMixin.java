ff0e2f8156349fcdaeff4c90db45f7f9badfe069
CC PCT <ccpct.co@gmail.com>
2026-06-30 02:03:00 +0100

    v26.1-1.0.0-alpha4
    - fix slabs and y dependent placements

--- src/main/java/CCPCT/bedrock_bridging/mixin/MinecraftMixin.java
+++ src/main/java/CCPCT/bedrock_bridging/mixin/MinecraftMixin.java
@@ -59,7 +59,7 @@ public class MinecraftMixin {
 
     @Unique
     private static Vec3 getHitVecFromPositions(BlockPos lastPlacePos, BlockPos target) {
-        return lastPlacePos.getCenter().lerp(target.getCenter(), 0.5);
+        return lastPlacePos.getCenter().lerp(target.getCenter(), 0.5).with(Direction.Axis.Y, magicY);
     }
 
     @Inject(method = "startUseItem", at = @At("HEAD"), cancellable = true)
